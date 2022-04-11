6b8610ed07f7e29c3ed09df7a10a4460d262e857
M1571 <94639639+M1571@users.noreply.github.com>
2022-04-11 18:32:17 +0200

    Hello World!

--- resources/views/helloWorld.blade.php
+++ resources/views/helloWorld.blade.php
@@ -5,8 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: royalblue;
+            color: white;
+        }
+
+        h1 {
+            text-align: center;
+            font-size: 3rem;
+            margin-top: 50px;
+        }
+
+    </style>
 </head>
 <body>
-    ciao
+    
+    <h1>Hello World!</h1>
+
 </body>
 </html>
