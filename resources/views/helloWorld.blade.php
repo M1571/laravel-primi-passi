52476491d72c61afcb6ab51d775ce365fcac527d
M1571 <94639639+M1571@users.noreply.github.com>
2022-04-11 18:36:26 +0200

    set comments

--- resources/views/helloWorld.blade.php
+++ resources/views/helloWorld.blade.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
+
+    <!-- ------------------------------------------------- -->
+    <!-- CSS -->
+
     <style>
         * {
             margin: 0;
@@ -24,10 +28,20 @@
         }
 
     </style>
+
+    <!-- CSS -->
+    <!-- ------------------------------------------------- -->
+
 </head>
 <body>
+
+    <!-- ------------------------------------------------- -->
+    <!-- HTML -->
     
     <h1>Hello World!</h1>
+
+    <!-- HTML -->
+    <!-- ------------------------------------------------- -->
 
 </body>
 </html>
